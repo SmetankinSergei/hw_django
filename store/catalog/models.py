c8d15a425c8f57879875259a044f823278d7082b
Smetankin <smet-eb@yandex.ru>
2024-01-09 11:26:19 +0300

    hw 19.2 done

--- store/catalog/models.py
+++ store/catalog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models import PROTECT
 
-# Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Name')
+    description = models.TextField()
+    img = models.ImageField(upload_to='')
+    category = models.ForeignKey('Category', on_delete=PROTECT, verbose_name='Category')
+    price = models.IntegerField(verbose_name='Price')
+    create_date = models.DateField(auto_now_add=True)
+    change_date = models.DateField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Name')
+    description = models.TextField()
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
--- store/store/settings.py
+++ store/store/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'store_db',
+        'USER': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
+        'PASSWORD': '1111'
     }
 }
 
